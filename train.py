3ef578ccaca45e2c296b8c56ce9ed83a4611648a
Graflinger <39885965+Graflinger@users.noreply.github.com>
2021-05-16 17:59:53 +0200

    Working version
    working version of jupyter notebook and python file

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+  
 from sklearn.linear_model import LogisticRegression
 import argparse
 import os
@@ -14,15 +15,7 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+ds = TabularDatasetFactory.from_delimited_files(['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'])
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +42,14 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df,y_df
+
+x,y = clean_data(ds)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -67,6 +67,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model_logreg.joblib')
 
 if __name__ == '__main__':
     main()
